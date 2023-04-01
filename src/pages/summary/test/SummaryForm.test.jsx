832dabfbe5bb065473075879c7aa40573db4ee36
Minh Nguyen <minhanh.nguyenquoc@gmail.com>
2023-04-01 15:08:53 -0400

    Mock service worker setup

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { screen, render, fireEvent } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -14,7 +15,8 @@ test("Initial conditions", () => {
   expect(confirmBtn).toBeDisabled();
 });
 
-test("Checkbox disables button on first click and enables on second click", () => {
+test("Checkbox disables button on first click and enables on second click", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
@@ -23,9 +25,32 @@ test("Checkbox disables button on first click and enables on second click", () =
     name: /confirm order/i,
   });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+  // popover starts out hidden/
+  // query - not in DOM
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  // get - in DOM
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
